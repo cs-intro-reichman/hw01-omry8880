4c9d76bdb1806051417a9d3502c1ad07779d3fdc
Omry Maor <omry.maor@runi.ac.il>
2023-12-18 19:50:29 +0200

    Completed AddTwo assignment.

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,10 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int firstInteger = Integer.parseInt(args[0]);
+		int secondInteger = Integer.parseInt(args[1]);
+		
+		System.out.println("The sum of the given integers is: " +
+		 (firstInteger + secondInteger));
 	}
 }
