dec0dc92be0f7867563e05055c19a0389ce19bea
Omry Maor <omry.maor@runi.ac.il>
2023-12-21 09:15:08 +0200

    Redone Gen3: removed use of Random class in favor of Math.random.

--- GenThree.java
+++ GenThree.java
@@ -4,15 +4,16 @@
  */
 import java.util.Random;
 
-public class GenThree {
+public class GenThree { 
 	public static void main(String[] args) {
 		Random rand = new Random();
 		int argsZero = Integer.parseInt(args[0]);
 		int argsOne = Integer.parseInt(args[1]);
-		
-		int randomOne = rand.nextInt(argsOne - argsZero) + argsZero; // range: starts from argsZero and ends in argsOne (argsZero -> argsOne)
-		int randomTwo = rand.nextInt(argsOne - argsZero) + argsZero;
-		int randomThree = rand.nextInt(argsOne - argsZero) + argsZero;
+
+		int randomOne = (int) (argsOne * Math.random() + argsZero); 
+		int randomTwo = (int) (argsOne * Math.random() + argsZero);
+		int randomThree = (int) (argsOne * Math.random() + argsZero);
+
 		int minRandom = Math.min(Math.min(randomOne, randomTwo), randomThree);
 
 		System.out.println(randomOne + "\n" + randomTwo + "\n" + randomThree + "\nThe minimum random is " + minRandom);
